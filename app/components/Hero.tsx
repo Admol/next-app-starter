f7b9b910f188db37c8fa880dd51a1e09c5c78750
wangjingling <wangjingling@yiyijk.com.cn>
2024-04-26 17:36:51 +0800

    update hero input

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -16,13 +16,17 @@ const Hero =  () => {
                         </p>
 
                         <div className="mt-5 lg:mt-8 flex flex-col items-center gap-2 sm:flex-row sm:gap-3">
-                            <div className="w-full sm:w-auto">
-                                <label htmlFor="hero-input" className="sr-only">
-                                    Search
-                                </label>
-                                <input type="text" id="hero-input" name="hero-input"
-                                       className="py-3 px-4 block w-full xl:min-w-72 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                                       placeholder="Enter your idea"></input>
+
+                            <div className="flex rounded-lg shadow-sm">
+                                <div
+                                    className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 dark:bg-neutral-700 dark:border-neutral-600">
+                                    <span className="text-sm text-gray-500 dark:text-neutral-400">https://example.com/</span>
+                                </div>
+                                <input type="text" name="hs-input-with-add-on-url" id="hs-input-with-add-on-url"
+                                       className="border-t py-3 px-4 pe-11 block w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                                       placeholder="your name">
+
+                                </input>
                             </div>
                             <a className="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
                                href="#">
