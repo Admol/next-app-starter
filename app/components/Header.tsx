158a77d34f43257fc14610f5541c8c910616658a
wangjingling <wangjingling@yiyijk.com.cn>
2024-04-26 15:31:40 +0800

    update header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,50 +4,71 @@ const Header = () => {
 
     return (
         <>
-            <div className="navbar  z-20 mt-2 mb-2 mx-auto flex h-14 w-full max-w-5xl flex-row flex-nowrap items-stretch justify-between bg-white py-3 duration-1000 ease-in-out animate-in fade-in slide-in-from-top-4 px-4 sm:px-6">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                      d="M4 6h16M4 12h8m-8 6h16"/>
+            <header
+                className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-4 dark:bg-neutral-800 drop-shadow-xl fixed top-0 left-0 z-50">
+                <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between"
+                     aria-label="Global">
+                    <div className="flex items-center justify-between">
+                        <a className="inline-flex items-center gap-x-2 text-xl font-semibold dark:text-white" href="/">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2"
+                                 stroke="currentColor" width="20" height="20">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"></path>
                             </svg>
+                            <h1 className="whitespace-nowrap">
+                                I Profile.page
+                            </h1>
+
+                        </a>
+                        <div className="sm:hidden">
+                            <button type="button"
+                                    className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10"
+                                    data-hs-collapse="#navbar-alignment" aria-controls="navbar-alignment"
+                                    aria-label="Toggle navigation">
+                                <svg className="hs-collapse-open:hidden flex-shrink-0 size-4"
+                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                     fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                     strokeLinejoin="round">
+                                    <line x1="3" x2="21" y1="6" y2="6"/>
+                                    <line x1="3" x2="21" y1="12" y2="12"/>
+                                    <line x1="3" x2="21" y1="18" y2="18"/>
+                                </svg>
+                                <svg className="hs-collapse-open:block hidden flex-shrink-0 size-4"
+                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                     fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                     strokeLinejoin="round">
+                                    <path d="M18 6 6 18"/>
+                                    <path d="m6 6 12 12"/>
+                                </svg>
+                            </button>
                         </div>
-                        <ul tabIndex={0}
-                            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li>
-                                <a className="text-lg">FAQ </a>
-                            </li>
-                            <li>
-                                <a>Pricing</a>
-                            </li>
-                        </ul>
+
                     </div>
-                    <a className="btn btn-ghost text-xl">
-                        <div className="relative inline-block">
-                            <img className="inline-block size-8 rounded"
-                                 src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"
-                                 alt="Image Description">
-                            </img>
+
+                    <div className="sm:order-3 whitespace-nowrap items-center gap-x-2 hidden sm:flex">
+                        <button type="button"
+                                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
+                            LOG IN
+                        </button>
+                    </div>
+                    <div id="navbar-alignment"
+                         className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
+                        <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:mt-0 sm:ps-5">
+                            <a className="font-medium text-blue-500" href="#" aria-current="page"></a>
+                            <a className="font-medium text-blue-500" href="#" aria-current="page"></a>
+                            <a className="font-medium text-blue-500" href="#" aria-current="page"></a>
+                            <a className="font-medium text-blue-500" href="#" aria-current="page"></a>
+                            <a className="font-medium text-blue-500" href="#" aria-current="page"></a>
+                            <a className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500"
+                               href="#">How To Use</a>
+                            <a className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500"
+                               href="#">FAQ</a>
+                            <a className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500"
+                               href="#">Pricing</a>
                         </div>
-                        <h1>App Star</h1>
-                    </a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1 text-lg">
-                        <li>
-                            <a href="/faq">FAQ </a>
-                        </li>
-                        <li>
-                            <a href="/pricing">Pricing</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    <a className="btn bg-neutral-100">LOG IN</a>
-                </div>
-            </div>
+                    </div>
+                </nav>
+            </header>
         </>
     );
 };
