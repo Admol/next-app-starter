47ca2a2bcc662b5bdced8ae081f7878afbf6f59a
wangjingling <wangjingling@yiyijk.com.cn>
2024-04-26 15:53:33 +0800

    add hero component

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,9 @@ import Hero from "@/app/components/Hero";
 export default function Home() {
   return (
     <>
-        <main className="flex min-h-screen flex-col items-center justify-between">
-            <div className="pl-14 pr-14 w-full bg-gradient-to-b from-transparent via-transparent ">
+        <main className="flex flex-col">
                 <Header/>
-                <Hero/>
-            </div>
+                <Hero />
         </main>
     </>
   );
